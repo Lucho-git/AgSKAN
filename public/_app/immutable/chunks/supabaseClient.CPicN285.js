aa51bb93ce1b30fabcb7f16c1c1b2bebf5782d46
Lachlan Ross <lachlan.f.ross@gmail.com>
2025-03-25 12:32:27 +0800

    Build

---
+++ public/_app/immutable/chunks/supabaseClient.CPicN285.js
@@ -0,0 +1,1 @@
+import{c as e,P as s,a as t}from"./index.DZXoKYNn.js";const r=e(t,s,{auth:{persistSession:!0,autoRefreshToken:!0,detectSessionInUrl:!0}});export{r as s};
---
+++ public/_app/immutable/entry/start.B5QzaOFD.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.DlOh58KF.js";export{a as start};
