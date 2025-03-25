50796ffba60f756a402f36b12eb10109f1b6cfe3
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2025-03-25 19:47:46 +0800

    added default vehicle icon

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -3,6 +3,7 @@ import location_pointer from "./location_pointer.svg?component"
 import location_drop from "./location_drop.svg?component"
 import locate_home from "./locate_home.svg?component"
 import bill_bold_duotone from "./bill_bold_duotone.svg?component"
+import vehicle_selection from "./vehicle_selection.svg?component"
 import skan from "./skan.svg?component"
 
 export default {
@@ -11,5 +12,6 @@ export default {
   location_pointer,
   location_drop,
   locate_home,
+  vehicle_selection,
   skan,
 }
