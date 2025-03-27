7a93268710a3f3724023dd22283a51f3750e183e
Lachlan Ross <lachlan.f.ross@gmail.com>
2025-03-27 19:04:48 +0800

    Bump main

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svelteSVG from 'vite-plugin-svelte-svg';
 export default defineConfig({
     plugins: [sveltekit(), svelteSVG({
         svgoConfig: {}, // See https://github.com/svg/svgo#configuration
-        requireSuffix: false, // Set to true to only import files ending in .svg?component
+        requireSuffix: false, // Set to true to only import files ending in .svg?components
     })],
     define: {
         'process.env.NODE_ENV': '"production"',
