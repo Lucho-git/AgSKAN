54ecd113d8820376c7e135e1cdbeab17e59ffaad
Lachlan Ross <lachlan.f.ross@gmail.com>
2025-04-17 13:21:13 +0800

    Remove public directory from Git tracking

--- public/_app/immutable/chunks/BoF3N7P3.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./CAuzrzLJ.js";const l=e({id:null,map_name:null,master_user_id:null,owner:null,is_owner:!1,masterSubscription:null,is_connected:!1});export{l as c};
--- public/_app/immutable/chunks/C58cqHmn.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./CAuzrzLJ.js";const o=t({marker_count:0,trail_count:0,connected_profiles:[],vehicle_states:[]});export{o as m};
