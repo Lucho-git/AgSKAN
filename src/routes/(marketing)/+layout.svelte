eab0bbb8da65da5785c4179a1c9633d33f0f9383
Lachlan Ross <lachlan.f.ross@gmail.com>
2025-10-13 13:39:09 +0800

    Update site with hotjar analytics

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -7,6 +7,10 @@
   export let data
 </script>
 
+<svelte:head>
+  <script src="https://t.contentsquare.net/uxa/e5382af3aa047.js"></script>
+</svelte:head>
+
 <div class="min-h-screen bg-background">
   <OgNavbar />
   <slot />
